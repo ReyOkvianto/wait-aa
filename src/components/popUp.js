598c87a40432671422f4007634a1e37d30fc0de8
Rey (Reyandra) Okvianto <55027338+ReyOkvianto@users.noreply.github.com>
2022-01-30 09:11:18 -0600

    Update popUp.js

--- src/components/popUp.js
+++ src/components/popUp.js
@@ -13,7 +13,7 @@ function Popup(props){
     const getPost = async () => {
         setPost([flight, destz, titles, image, reviews, tag]);
         if(postInfo !== []) {
-            fetch('https://wait-aa-api.herokuapp.com/post/' + flight+ "/" + destz + "/" + titles + "/" + image + "/" + reviews + "/" + tag).then(function(response) {
+            fetch('https://wait-aa-api.herokuapp.com/post/' + flight + "/" + destz + "/" + titles + "/" + image + "/" + reviews + "/" + tag).then(function(response) {
                 return response.text();
               }).then(function(data) {
                 console.log(data)
@@ -65,41 +65,36 @@ function Popup(props){
     };
 
     return(props.trigger) ? (
-        <div>
+        <div className="search-box">
             <form onSubmit={getSearch}>
-                <label>
-                Flight Number: 
-                <input type="text" name="flight_num" onChange={handleText}
+                <div className="search-box">
+                    <input className="search-bar" placeholder="Flight Number" type="text" name="flight_num" onChange={handleText}/>
+                </div>              
+                <div className="search-box">
+                    <input className="search-bar" placeholder="Destination..." type="text" name="dest"                   onChange={handledest}/>
+                </div>
+                <div className="search-box">
+                    <input className="search-bar" placeholder="Title..." type="text" name="title"                   onChange={handletitle}/>
+                </div>
+                
+                <div className="search-box">
+                    <input className="search-bar" placeholder="Image URL..." type="text" name="image_url"                   onChange={handleimage}/>
+                </div>              
+
+                <div className="search-box">
+
+                <input className="search-bar" placeholder="Reviews..." type="text" name="review"                   onChange={handlereview}
 />
-                </label>
-                <label>
-                Destination: 
-                </label>
-                <input type="text" name="dest"                   onChange={handledest}
+                </div>
+                <div className="search-box">
+
+                <input className="search-bar" placeholder="Tags..." type="text" name="tags"                   onChange={handletag}
 />
-                <label>
-                Title: 
-                <input type="text" name="title"                   onChange={handletitle}
-/>
-                </label>
-                <label>
-                Image URL: 
-                <input type="text" name="image_url"                   onChange={handleimage}
-/>
-                </label>
-                <label>
-                Reviews: 
-                <input type="text" name="review"                   onChange={handlereview}
-/>
-                </label>
-                <label>
-                Tags: 
-                <input type="text" name="tags"                   onChange={handletag}
-/>
-                </label>
-                <button >Submit</button>
-                <button onClick = {()=> props.setTrigger(false)}>Close</button>
-                {props.children}
+                </div>
+                    <button className="center-button">Submit</button>
+                    <button onClick = {()=> props.setTrigger(false)} className="center-red">Close</button>
+                    {props.children}
+                
             </form>
         </div>
         
